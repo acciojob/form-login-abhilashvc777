96713f6c015ecfdbf8ab20ecff2eb54e7a316a52
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-19 01:23:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
+let form = document.querySelector("#form1")
+const input = document.querySelectorAll(".a")
+function getFormvalue(e) {
     //Write your code here
-
+	let bag = ""
+  for(let i=0; i<=input.length-1; i++){
+	  bag = bag+input[i].value+" "
+  }
+   let g = bag.replace(/^\s+|\s+$/gm,'')
+	return alert(g)
 }
